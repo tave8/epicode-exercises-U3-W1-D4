8795ca9d0fcf41f8881bd0813a3741ca46818ec6
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-05 16:13:42 +0100

    AddComment add form

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,8 +1,39 @@
 import { Component } from "react"
+import { Container, Col, Form, Row, Button } from "react-bootstrap"
 
 class AddComment extends Component {
   render() {
-    return <textarea placeholder={`How did you find ${this.props.book.title}?`}></textarea>
+    return (
+      <>
+        <Container>
+          <Row>
+            <Form>
+              <Col>
+                {/* review rating */}
+                <Form.Group>
+                  <Form.Label>Choose rating</Form.Label>
+                  <Form.Select defaultValue={1}>
+                    <option value="1">1</option>
+                    <option value="1">2</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                {/* review text */}
+                <Form.Group>
+                  <Form.Label>Review</Form.Label>
+                  <Form.Control placeholder={`How did you find ${this.props.book.title}?`} style={{ minHeight: "200px" }} />
+                </Form.Group>
+              </Col>
+
+              {/* submit review button */}
+              <Button type="submit">Submit</Button>
+            </Form>
+          </Row>
+        </Container>
+      </>
+    )
   }
 }
 
