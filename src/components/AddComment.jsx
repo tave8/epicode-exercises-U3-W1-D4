65377ce6b5acab19c3092892b914a4bd61fc0aa6
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-05 16:36:02 +0100

    can add reviews

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,22 +1,56 @@
 import { Component } from "react"
 import { Container, Col, Form, Row, Button } from "react-bootstrap"
 
+const initialReview = {
+  rate: "5",
+  comment: "",
+}
+
 class AddComment extends Component {
   state = {
-    formValues: {
-      rating: "5",
-      comment: "",
-    },
+    formValues: initialReview,
   }
 
   onFormSubmit = (event) => {
     event.preventDefault()
+
+    // the asin of the current book
+    const bookAsin = this.props.book.asin
+
+    const reviewToAdd = { ...this.state.formValues, elementId: bookAsin }
+
+    const apiUrl = `https://striveschool-api.herokuapp.com/api/comments`
+
+    const config = {
+      method: "POST",
+      body: JSON.stringify(reviewToAdd),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTczMmNiMjg1ZTNiMTAwMTViNWVkNzgiLCJpYXQiOjE3NjkxNTU3NjMsImV4cCI6MTc3MDM2NTM2M30.BxSnAzs-jRVXg8PvTmmmQNOcI4pkhyHK6ADl2JGsKzk",
+      },
+    }
+
+    fetch(apiUrl, config)
+      .then((response) => {
+        if (response.ok) {
+          alert("Review successfully added")
+          this.setState({
+            formValues: initialReview,
+          })
+        } else {
+          throw new Error("Error while adding review")
+        }
+      })
+      .catch((err) => {
+        console.log("error while saving", err)
+      })
   }
 
-  onRatingChange = (event) => {
-    const newRating = event.target.value
+  onRateChange = (event) => {
+    const newRate = event.target.value
     this.setState({
-      formValues: { ...this.state.formValues, rating: newRating },
+      formValues: { ...this.state.formValues, rate: newRate },
     })
   }
 
@@ -34,10 +68,10 @@ class AddComment extends Component {
           <Row>
             <Form onSubmit={this.onFormSubmit}>
               <Col>
-                {/* review rating */}
+                {/* review rate */}
                 <Form.Group>
-                  <Form.Label>Choose rating</Form.Label>
-                  <Form.Select onChange={this.onRatingChange} value={this.state.formValues.rating}>
+                  <Form.Label>Choose rate</Form.Label>
+                  <Form.Select onChange={this.onRateChange} value={this.state.formValues.rate}>
                     <option value="5">5</option>
                     <option value="4">4</option>
                     <option value="3">3</option>
@@ -51,7 +85,12 @@ class AddComment extends Component {
                 {/* review text */}
                 <Form.Group>
                   <Form.Label>Review</Form.Label>
-                  <Form.Control onChange={this.onCommentChange} value={this.state.formValues.comment} placeholder={`How did you find ${this.props.book.title}?`} style={{ minHeight: "200px" }} />
+                  <Form.Control
+                    onChange={this.onCommentChange}
+                    value={this.state.formValues.comment}
+                    placeholder={`How did you find ${this.props.book.title}?`}
+                    style={{ minHeight: "200px" }}
+                  />
                 </Form.Group>
               </Col>
 
