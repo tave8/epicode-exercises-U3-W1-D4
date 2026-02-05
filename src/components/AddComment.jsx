627a4483e5975d918156866abce034519fbab213
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-05 16:24:03 +0100

    2-way data binding with AddComment form

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -2,19 +2,47 @@ import { Component } from "react"
 import { Container, Col, Form, Row, Button } from "react-bootstrap"
 
 class AddComment extends Component {
+  state = {
+    formValues: {
+      rating: "5",
+      comment: "",
+    },
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  onRatingChange = (event) => {
+    const newRating = event.target.value
+    this.setState({
+      formValues: { ...this.state.formValues, rating: newRating },
+    })
+  }
+
+  onCommentChange = (event) => {
+    const newComment = event.target.value
+    this.setState({
+      formValues: { ...this.state.formValues, comment: newComment },
+    })
+  }
+
   render() {
     return (
       <>
         <Container>
           <Row>
-            <Form>
+            <Form onSubmit={this.onFormSubmit}>
               <Col>
                 {/* review rating */}
                 <Form.Group>
                   <Form.Label>Choose rating</Form.Label>
-                  <Form.Select defaultValue={1}>
+                  <Form.Select onChange={this.onRatingChange} value={this.state.formValues.rating}>
+                    <option value="5">5</option>
+                    <option value="4">4</option>
+                    <option value="3">3</option>
+                    <option value="2">2</option>
                     <option value="1">1</option>
-                    <option value="1">2</option>
                   </Form.Select>
                 </Form.Group>
               </Col>
@@ -23,7 +51,7 @@ class AddComment extends Component {
                 {/* review text */}
                 <Form.Group>
                   <Form.Label>Review</Form.Label>
-                  <Form.Control placeholder={`How did you find ${this.props.book.title}?`} style={{ minHeight: "200px" }} />
+                  <Form.Control onChange={this.onCommentChange} value={this.state.formValues.comment} placeholder={`How did you find ${this.props.book.title}?`} style={{ minHeight: "200px" }} />
                 </Form.Group>
               </Col>
 
